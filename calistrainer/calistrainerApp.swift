ead51353ad95c82184f5ae62d2ac20b6b83cc312
Eibiel Sardjanto <mohamad.eibiel@icehousecorp.com>
2022-03-14 20:06:27 +0700

    no message

---
+++ calistrainer/calistrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  calistrainerApp.swift
+//  calistrainer
+//
+//  Created by Eibiel Sardjanto on 14/03/22.
+//
+
+import SwiftUI
+
+@main
+struct calistrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
