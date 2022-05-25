ee5a533738e089b4b65d7de4397ab7e9b09d183d
Eibiel Sardjanto <mohamad.eibiel@icehousecorp.com>
2022-05-25 20:47:04 +0700

    Implemented exercise log view and view model

--- calistrainer/ExerciseLogs/ExerciseLogsView.swift
+++ calistrainer/ExerciseLogs/ExerciseLogsView.swift
@@ -11,30 +11,98 @@ struct ExerciseLogsView: View {
 
 	@StateObject var viewModel = ExerciseLogsViewModel()
 	
-    var body: some View {
-		ScrollView {
-			Text(String(viewModel.sessions.count))
-			VStack {
-				ForEach(viewModel.sessions, id: \.self) { session in
+	var body: some View {
+		ZStack {
+			Color("PrimaryGray").ignoresSafeArea()
+			VStack(spacing: 0) {
+				VStack {
 					HStack {
-						Text(session.value(forKey: "exercise") as! String)
+						Button {
+							viewModel.prevMonthButtonTapped()
+							viewModel.updateDatesInMonth()
+							viewModel.fetchRecordedSessions()
+						} label: {
+							Image(systemName: "chevron.left")
+								.foregroundColor(.white)
+						}.padding(.horizontal)
 						Spacer()
-						Text(viewModel.castIntAsString(value: session.value(forKey: "repetitionCount") as! Int16))
-					}
-					.foregroundColor(.white)
-					.padding()
+						Text(viewModel.selectedMonth)
+							.font(.title3)
+							.fontWeight(.semibold)
+						Spacer()
+						Button {
+							viewModel.nextMonthButtonTapped()
+							viewModel.updateDatesInMonth()
+							viewModel.fetchRecordedSessions()
+						} label: {
+							Image(systemName: "chevron.right")
+								.foregroundColor(
+									viewModel.isLatestMonthSelected
+									? Color("PrimaryGray")
+									: .white
+								)
+						}.padding(.horizontal)
+							.disabled(viewModel.isLatestMonthSelected)
+					}.padding()
+					ScrollView(.horizontal, showsIndicators: false) {
+						ScrollViewReader { value in
+							HStack {
+								ForEach(Array(zip(viewModel.datesInMonth.indices, viewModel.datesInMonth)), id: \.0) { index, date in
+									DateButton(
+										date: date,
+										buttonState: viewModel.dateButtonState(date),
+										action: {
+											viewModel.selectDate(date)
+											viewModel.showSessionsForSelectedDate()
+										}
+									)
+									.disabled(viewModel.disableDateButton(date))
+									.id(index)
+									.onAppear {
+										value.scrollTo(viewModel.selectedDateIndex, anchor: .bottomTrailing)
+									}
+								}
+							}.padding()
 
+						}
+					}
+					.padding(.bottom)
+				}
+				.background {
+					RoundedRectangle(cornerRadius: 20)
+						.foregroundColor(Color("SecondaryGray"))
+						.ignoresSafeArea()
+				}
+				if viewModel.sessionsForSelectedDate.isEmpty {
+					Spacer()
+					Text(viewModel.emptyListLabel)
+						.font(.title3)
+						.foregroundColor(.accentColor)
+						.multilineTextAlignment(.center)
+						.lineSpacing(10)
+					Spacer()
+				} else {
+					ScrollView {
+						VStack(spacing: 20) {
+							ForEach(viewModel.sessionsForSelectedDate, id:\.self) { practiceSession in
+								ExerciseLogItem(logItem: viewModel.getLogItem(from: practiceSession))
+							}
+						}.padding()
+					}
 				}
 			}
 		}
 		.onAppear {
 			viewModel.fetchRecordedSessions()
+			viewModel.showSessionsForSelectedDate()
+			viewModel.updateDatesInMonth()
 		}
-    }
+	}
 }
 
 struct ExerciseLogsView_Previews: PreviewProvider {
-    static var previews: some View {
-        ExerciseLogsView()
-    }
+	static var previews: some View {
+		ExerciseLogsView()
+			.preferredColorScheme(.dark)
+	}
 }
